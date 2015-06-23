0646744a843c6a13bb8f207e3868ab28548474c0
SBALBA1 <lbalbastre@opentrends.net>
2015-06-23 13:57:47 +0200

    Exercices from second week

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Cache the inverse of a matrix
+
+## This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
+}
+
+
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above.
+
+cacheSolve <- function(x, ...) {
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+          message("getting cached data")
+          return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data)
+        x$setinverse(inverse)
+        inverse
+}
